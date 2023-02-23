b2e40aafbcbc4dc7b5163f3ae1f225edce8e6d2b
Alex Nemeth <nemeth.alex@proton.me>
2023-02-23 12:38:16 -0800

    Exercise 3

--- src/components/PreviousGuessList/PreviousGuessList.js
+++ src/components/PreviousGuessList/PreviousGuessList.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 function PreviousGuessList({ previousGuesses }) {
-  return (
-    <div className="guess-results">
-      {previousGuesses.map((guess, index) => (
-        <p key={index} className="guess">
-          {guess}
-        </p>
+  const guessCells = previousGuesses.map((guess, index) => (
+    <p className="guess" key={index}>
+      {Array.from(guess).map((letter, index) => (
+        <span className="cell" key={index}>
+          {letter}
+        </span>
       ))}
-    </div>
+    </p>
+  ));
+  const blankCells = range(previousGuesses.length, NUM_OF_GUESSES_ALLOWED).map(
+    (key) => (
+      <p className="guess" key={key}>
+        {range(5).map((_, index) => (
+          <span className="cell" key={index}></span>
+        ))}
+      </p>
+    )
   );
+
+  return <div className="guess-results">{[...guessCells, ...blankCells]}</div>;
 }
 
 export default PreviousGuessList;
